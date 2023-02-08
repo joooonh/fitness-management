9ac6a44924a2adc31d7543c97433d343c95d0f00
sungmin <castlemin222@naver.com>
2023-02-08 17:47:40 +0900

    프로그램 조회 목록 추가

---
+++ src/main/java/com/example/mapper/ProgramMapper.java
@@ -0,0 +1,29 @@
+package com.example.mapper;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.example.dto.ProgramDto;
+import com.example.vo.Program;
+import com.example.vo.ProgramCategory;
+import com.example.vo.ProgramDay;
+import com.example.vo.User;
+
+@Mapper
+public interface ProgramMapper {
+	
+	// 모든 프로그램 목록
+	List<Program> getAllPrograms(Map<String, Object> param);
+	int getTotalRows(Map<String, Object> rows);
+	// 프로그램 상세정보
+	ProgramDto getProgramDetail(int programNo);
+	// 프로그램 진행 요일
+	List<ProgramDay> getProgramDayByProgramNo(int programNo);
+	// 프로그램 신청자 목록
+	List<User> getUsersByProgramNo(int programNo);
+	// 프로그램 분류 목록
+	List<ProgramCategory> getProgramCategory();
+	
+}
