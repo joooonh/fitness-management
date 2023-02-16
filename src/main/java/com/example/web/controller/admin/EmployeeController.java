0db76a53f0d7e7419887a820b9ba52f4c71d60a3
sungmin <castlemin222@naver.com>
2023-02-16 11:47:54 +0900

    직원 관리 조회 목록 추가

--- src/main/java/com/example/web/controller/admin/EmployeeController.java
+++ src/main/java/com/example/web/controller/admin/EmployeeController.java
@@ -3,6 +3,7 @@ package com.example.web.controller.admin;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.Map;
 
 import javax.validation.Valid;
 
@@ -17,11 +18,13 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.example.dto.EmployeeDetail;
 import com.example.service.admin.EmployeeService;
+import com.example.vo.Employee;
 import com.example.web.request.EmployeeModifyForm;
 
 @Controller
@@ -33,6 +36,27 @@ public class EmployeeController {
 	
 	@Autowired
 	EmployeeService employeeService;
+	
+	// 직원 조회
+	@GetMapping("/list")
+	public String list(@RequestParam(name = "page", required = false, defaultValue = "1") int page, 
+			@RequestParam(name = "status", required = false, defaultValue = "") String status,
+			@RequestParam(name = "keyword", required = false, defaultValue = "") String keyword, Model model) {
+		Map<String, Object> result = employeeService.getAllEmployee(page,status,keyword);
+		// 직원 목록 정보
+		model.addAttribute("employees", result.get("employees"));
+		// 페이징처리에 필요한 정보
+		model.addAttribute("pagination", result.get("pagination"));
+		
+		return "admin/employee/list";
+	}
+	
+	// 아이디로 직원 정보 조회
+	@GetMapping("/detail.json")
+	@ResponseBody
+	public Employee detail(@RequestParam(name = "empId") String empId) {
+		return employeeService.getEmployeeById(empId);
+	}
 	
 	// 내 정보 조회
 	@GetMapping("/mypage")
