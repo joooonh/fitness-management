d637a83c29c68b72798579c95d7d815727616005
mecuri <sdy9404@gmail.com>
2023-03-02 20:31:45 +0900

    Fix: 충돌 병합

--- src/main/java/com/example/utils/DateUtils.java
+++ src/main/java/com/example/utils/DateUtils.java
@@ -6,41 +6,27 @@ import java.util.Date;
 
 public class DateUtils {
 
-	
-	private static SimpleDateFormat sdf1 = new SimpleDateFormat("yyyy-MM-dd HH:mm");
-	private static SimpleDateFormat sdf2 = new SimpleDateFormat("yyyy-MM-dd");
-	
+
+    private static SimpleDateFormat sdf1 = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+    private static SimpleDateFormat sdf2 = new SimpleDateFormat("yyyy-MM-dd");
 
 
-
-	private static SimpleDateFormat sdf1 = new SimpleDateFormat("yyyy-MM-dd HH:mm");
-	private static SimpleDateFormat sdf2 = new SimpleDateFormat("yyyy-MM-dd");
-
-
-	public static Date textToDate(Date date) {
-		return textToDate(date, "00:00");
-	}
+    public static Date textToDate(Date date) {
+        return textToDate(date, "00:00");
+    }
 
 
-	public static Date textToDate(Date date, String time) {
-		try {
-			if (time == null) {
-				return date;
-			}
-			
+    public static Date textToDate(Date date, String time) {
+        try {
+            if (time == null) {
+                return sdf1.parse(sdf2.format(date) + " 00:00");
+            }
 
-
-	public static Date textToDate(Date date, String time) {
-		try {
-			if (time == null) {
-				return sdf1.parse(sdf2.format(date) + " 00:00");
-			}
-
-			return sdf1.parse(sdf2.format(date) + " " + time);
-		} catch (ParseException ex) {
-			throw new RuntimeException(ex);
-		}
-	}
+            return sdf1.parse(sdf2.format(date) + " " + time);
+        } catch (ParseException ex) {
+            throw new RuntimeException(ex);
+        }
+    }
 
 }
 
