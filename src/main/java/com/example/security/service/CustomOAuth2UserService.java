3da50840c8ca03f68fd3b3ebf8b4017769f9b14e
junhee <joooonh12@gmail.com>
2023-03-02 17:07:30 +0900

    Feat: 소셜로그인 기능

--- src/main/java/com/example/security/service/CustomOAuth2UserService.java
+++ src/main/java/com/example/security/service/CustomOAuth2UserService.java
@@ -1,23 +1,22 @@
 package com.example.security.service;
 
-import java.util.Collections;
-
-import javax.servlet.http.HttpSession;
+import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
-import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
 import com.example.mapper.UserMapper;
-import com.example.security.vo.OAuthAttributes;
-import com.example.security.vo.SessionUser;
+import com.example.security.oauth.info.OAuth2UserInfo;
+import com.example.security.oauth.info.OAuth2UserInfoFactory;
+import com.example.security.vo.CustomOAuth2User;
+import com.example.service.user.UserService;
 import com.example.vo.User;
+import com.example.vo.UserRole;
 
 @Service
 public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequest, OAuth2User>{
@@ -25,49 +24,60 @@ public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequ
 	@Autowired
 	private UserMapper userMapper;
 	@Autowired
-	private HttpSession httpSession;
+	private UserService userService;
 	
 	@Override
 	public OAuth2User loadUser(OAuth2UserRequest oAuth2UserRequest) throws OAuth2AuthenticationException {
-		OAuth2UserService oAuth2UserService = new DefaultOAuth2UserService();
+		OAuth2UserService<OAuth2UserRequest, OAuth2User> oAuth2UserService = new DefaultOAuth2UserService();
 		OAuth2User oAuth2User = oAuth2UserService.loadUser(oAuth2UserRequest);
 		
-		// 현재 진행중인 OAuth2 서비스를 구분. (구글, 카카오, 네이버) {registrationId='naver'} 이런식으로
-        String registrationId = oAuth2UserRequest.getClientRegistration().getRegistrationId();
+		// 현재 진행중인 OAuth2 서비스를 구분. (구글, 카카오, 네이버) {providerType='naver'} 
+        String providerType = oAuth2UserRequest.getClientRegistration().getRegistrationId();
         
         // OAuth2 로그인 시 키 값이 된다. 구글은 키 값이 "sub"이고, 네이버는 "response"이고, 카카오는 "id"이다. 각각 다르므로 이렇게 따로 변수로 받아서 넣어줘야함.
-        String userNameAttributeName = oAuth2UserRequest.getClientRegistration().getProviderDetails().getUserInfoEndpoint().getUserNameAttributeName();
+      	OAuth2UserInfo userInfo = OAuth2UserInfoFactory.getOAuth2UserInfo(providerType, oAuth2User.getAttributes());
+        User savedUser = userMapper.getUserById(userInfo.getId());
         
-        // OAuth2 로그인을 통해 가져온 OAuth2User의 attribute를 담아주는 of 메소드.
-        OAuthAttributes attributes = OAuthAttributes.of(registrationId, userNameAttributeName, oAuth2User.getAttributes());
+        if(savedUser != null) {
+        	if(!providerType.equals(savedUser.getProviderType())) {
+        		throw new OAuth2AuthenticationException("소셜로그인 공급자가 일치하지 않습니다.");
+        	}
+        	updateUser(savedUser, userInfo);
+        } else {
+        	savedUser = createUser(userInfo, providerType);
+        }
+		return new CustomOAuth2User(savedUser, userInfo.getAttributes());
+        	
+	}
 
-	        User user = saveOrUpdate(attributes);
-	        httpSession.setAttribute("user", new SessionUser(user));
-	
-	        System.out.println(attributes.getAttributes());
-	        return new DefaultOAuth2User(Collections.singleton(new SimpleGrantedAuthority("ROLE_USER"))
-	                , attributes.getAttributes()
-	                , attributes.getNameAttributeKey());
-	    }
-	
-		// 이미 저장된 정보라면, update 처리
-	    private User saveOrUpdate(OAuthAttributes attributes) {
-	        User user = userMapper.getUserByEmail(attributes.getEmail());
-	        if(user != null) {
-	        	user.setName(attributes.getName());
-	        	user.setPhoto(attributes.getPhoto());
-	        	
-	        	userMapper.updateUser(user);
-	        	return user;
-	        } else {
-	        	User newUser = new User();
-	        	newUser.setName(attributes.getName());
-	        	newUser.setEmail(attributes.getEmail());
-	        	newUser.setPhoto(attributes.getPhoto());
-	        	
-	        	userMapper.insertUser(newUser);
-	        	return newUser;
-	        }
-	
-	    }
+	private User createUser(OAuth2UserInfo userInfo, String providerType) {
+		User user = new User();
+		UserRole userRole = new UserRole();
+		user = userService.getUserinfo(userInfo.getId());
+		
+		user.setId(userInfo.getId());
+		user.setName(userInfo.getName());
+		user.setEmail(userInfo.getEmail());
+		user.setProviderType(providerType);
+		user.setCreatedDate(new Date());
+		user.setUpdatedDate(new Date());
+		
+		userRole.setUserNo(user.getNo());
+		userRole.setRoleName("ROLE_USER");
+		
+		userMapper.insertUser(user);
+		
+		return user;
+	}
+
+	// 이미 저장된 정보라면, update 처리
+    private User updateUser(User user, OAuth2UserInfo userInfo) {
+    	
+    	if(userInfo.getName() != null && !user.getName().equals(userInfo.getName())) {
+    		user.setName(userInfo.getName());
+    	}
+    	userMapper.updateUser(user);
+    	
+    	return user;
+    }
 }
