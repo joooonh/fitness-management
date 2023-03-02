b455a3c44b7dca27abf53f2c9417fba2d00948de
ChocolateDukoo <104629002+ChocolateDukoo@users.noreply.github.com>
2023-03-02 19:45:44 +0900

    Revert "Revert "feat : 강사일정관리, 프로그램 일정관리 페이지 기능 추가""

--- src/main/java/com/example/mapper/EmployeeScheduleMapper.java
+++ src/main/java/com/example/mapper/EmployeeScheduleMapper.java
@@ -1,14 +1,20 @@
 package com.example.mapper;
 
 import java.util.List;
+import java.util.Map;
 
 import org.apache.ibatis.annotations.Mapper;
 
+import com.example.dto.EmployeeScheduleDto;
+import com.example.dto.ProgramCalendarDto;
 import com.example.vo.Employee;
 
 @Mapper
 public interface EmployeeScheduleMapper {
 
-	Employee getEmployeeById(String employeeId);
+	EmployeeScheduleDto getEmployeeById(String employeeId);
 	List<Employee> getAllEmployees();
+	
+	List<EmployeeScheduleDto> getEmployeeEvents(Map<String, Object> param);
+	EmployeeScheduleDto getEmployeeEventsById(String employeeId);
 }
--- src/main/java/com/example/utils/DateUtils.java
+++ src/main/java/com/example/utils/DateUtils.java
@@ -6,9 +6,16 @@ import java.util.Date;
 
 public class DateUtils {
 
+	
+	private static SimpleDateFormat sdf1 = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+	private static SimpleDateFormat sdf2 = new SimpleDateFormat("yyyy-MM-dd");
+	
+
+
 
 	private static SimpleDateFormat sdf1 = new SimpleDateFormat("yyyy-MM-dd HH:mm");
 	private static SimpleDateFormat sdf2 = new SimpleDateFormat("yyyy-MM-dd");
+
 
 	public static Date textToDate(Date date) {
 		return textToDate(date, "00:00");
@@ -18,11 +25,24 @@ public class DateUtils {
 	public static Date textToDate(Date date, String time) {
 		try {
 			if (time == null) {
+				return date;
+			}
+			
+
+
+	public static Date textToDate(Date date, String time) {
+		try {
+			if (time == null) {
 				return sdf1.parse(sdf2.format(date) + " 00:00");
 			}
+
 			return sdf1.parse(sdf2.format(date) + " " + time);
 		} catch (ParseException ex) {
 			throw new RuntimeException(ex);
 		}
 	}
+
 }
+
+}
+
