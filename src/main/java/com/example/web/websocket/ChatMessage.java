7e6c26142cf24861b980d4eb30daadd0c1a33ec7
junhee <joooonh12@gmail.com>
2023-03-02 09:42:07 +0900

    Fix: 채팅 기능 오류 수정

--- src/main/java/com/example/web/websocket/ChatMessage.java
+++ src/main/java/com/example/web/websocket/ChatMessage.java
@@ -10,6 +10,12 @@ import lombok.ToString;
 public class ChatMessage {
 
 	// 메시지 형태 - 채팅 명령
+	// chat-open : 상담연결 요청
+	// chat-close : 상담종료 요청
+	// chat-open-success : 상담연결 성공 응답
+	// chat-close-success : 상담종료 성공 응답
+	// chat-message : 상담 메시지 교환 요청 및 응답
+	// chat-error : 에러 응답
 	private String cmd;
 	private String roomId;
 	private String customerId;
