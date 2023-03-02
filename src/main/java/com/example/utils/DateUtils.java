5703c3f23486442d0969e7b2e8cd58e620b82b92
ChangWoo <hcw6426@naver.com>
2023-03-02 16:16:02 +0900

    feat : 강사일정관리, 프로그램 일정관리 페이지 기능 추가
    강사일정관리 페이지에서 강사명 검색 시 해당 월에 속한 프로그램이 조회되게 하였습니다.
    프로그램 일정관리 페이지에서 프로그램명 검색 시 해당 월에 속한 프로그램이 조회되게 하였습니다.

---
+++ src/main/java/com/example/utils/DateUtils.java
@@ -0,0 +1,27 @@
+package com.example.utils;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class DateUtils {
+	
+	private static SimpleDateFormat sdf1 = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+	private static SimpleDateFormat sdf2 = new SimpleDateFormat("yyyy-MM-dd");
+	
+	public static Date textToDate(Date date) {
+		return textToDate(date, "00:00");
+	}
+
+	public static Date textToDate(Date date, String time) {
+		try {
+			if (time == null) {
+				return date;
+			}
+			
+			return sdf1.parse(sdf2.format(date) + " " + time);
+		} catch (ParseException ex) {
+			throw new RuntimeException(ex);
+		}
+	}
+}
